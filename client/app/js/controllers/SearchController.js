55d25f1e5c4a61a2f8c5df1a1a6a1b2e75b61099
Brian P. Johnson <bjohnson@trulia.com>
2013-06-28 13:51:08 -0700

    Fixing search filter

--- client/app/js/controllers/SearchController.js
+++ client/app/js/controllers/SearchController.js
@@ -2,7 +2,7 @@
  * SearchController
  */
 define ([], function () {
-  return ['$scope', '$http', 'listings', 'geocoder', 'listings', '$rootScope', function ($scope, $http, listings, geocoder, $rootScope) {
+  return ['$scope', '$http', 'listings', 'geocoder', 'listings', function ($scope, $http, listings, geocoder) {
     geocoder.getCurLocation (function (err, loc) {
       $scope.geoCode = loc.geocode;
       $scope.locations = loc.addresses;
