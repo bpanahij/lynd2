761e74152f6fcaf3ec81dba888d3f15242a64605
Brian P. Johnson <bjohnson@trulia.com>
2013-06-27 22:46:38 -0700

    Fixing placeholder picture

--- client/app/js/controllers/ListingController.js
+++ client/app/js/controllers/ListingController.js
@@ -4,7 +4,6 @@
 define([], function() {
   return ['$scope', '$http', '$routeParams', 'listings', function($scope, $http, $routeParams, listings) {
     $scope.listingId = $routeParams.listingId;
-
     listings.getById(false, $scope.listingId, {lat:0, long:0}, function(listing) {
       $scope.listing = listing;
       $scope.$apply();
